617f225f733b83b9d61dd5069ea257bdef27c9f3
Delitz Vincent <vincent.delitz@tum.de>
2019-07-17 16:18:00 +0000

    our application is now deployable with the correct UI

--- UI/Gruntfile.js
+++ UI/Gruntfile.js
@@ -1,15 +1,8 @@
 module.exports = function (grunt) {
 	"use strict";
 	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
-	grunt.config.merge({
-		compatVersion: "edge",
-		deploy_mode: "html_repo"
-	});
-	grunt.registerTask("default", [
-		"clean",
-		"lint",
-		"build"
-	]);
+	grunt.config.merge({ compatVersion: "edge" });
+	grunt.registerTask("default", ["lint"]);
 	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-test");
 	grunt.registerTask("unit_and_integration_tests", ["test"]);
 	grunt.config.merge({
